f33543ffa02b9e0e61a811c6d4f6720fd6ba45df
Mercesletifer47 <art-matv98@yandex.ru>
2021-08-02 18:56:17 +0300

    Actual

--- src/ru/geekbrains/HomeWorkApp_java2_lesson4/Main.java
+++ src/ru/geekbrains/HomeWorkApp_java2_lesson4/Main.java
@@ -7,15 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Chat");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 400, 388));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
